dcfe6a513e17c7dc7df55d6e0c4d4c661f027da1
azimkhan <wanrossen@gmail.com>
2019-08-08 18:37:43 +0600

    tests

--- gocopy_test.go
+++ gocopy_test.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"gotest.tools/assert"
+	"os"
 	"testing"
 )
 
 func TestCopy(t *testing.T) {
-	var err error
-	err = Copy("src", "dest", 2048, 0)
-	assert.NilError(t, err)
+	var fileErr error
+	var copyErr error
+	var n int
+	var file *os.File
+	buf := make([]byte, 1)
 
-	err = Copy("src", "dest", 2048, 1024)
-	assert.NilError(t, err)
+	// === Copy without offset
+	copyErr = Copy("src", "dest", 2048, 0)
+	assert.NilError(t, copyErr)
+	file, fileErr = os.Open("dest")
+	assert.NilError(t, fileErr)
+
+	// check first and last byte of a file
+	n, fileErr = file.ReadAt(buf, int64(2047))
+	assert.NilError(t, fileErr)
+	assert.Equal(t, 1, n)
+	assert.Equal(t, uint8(10), buf[0]) // should be LF
+
+	n, fileErr = file.ReadAt(buf, int64(0))
+	assert.NilError(t, fileErr)
+	assert.Equal(t, 1, n)
+	assert.Equal(t, uint8(97), buf[0]) // should be "a"
+
+	// === Copy with offset
+	copyErr = Copy("src", "dest", 2048, 1024)
+	assert.NilError(t, copyErr)
+	file, fileErr = os.Open("dest")
+	assert.NilError(t, fileErr)
+
+	// check first and last byte of a file
+	n, fileErr = file.ReadAt(buf, int64(1023))
+	assert.NilError(t, fileErr)
+	assert.Equal(t, 1, n)
+	assert.Equal(t, uint8(10), buf[0]) // should be LF
+
+	n, fileErr = file.ReadAt(buf, int64(0))
+	assert.NilError(t, fileErr)
+	assert.Equal(t, 1, n)
+	assert.Equal(t, uint8(99), buf[0]) // should be "c"
 }
